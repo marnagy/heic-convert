cafd73cacbd1f6f49e58462889eba1da4a8bd875
Marek Nagy <62247071@cuni.cz>
2021-07-08 23:28:53 +0200

    Added optional argument of threads, default=1

--- HeicConvert/Program.cs
+++ HeicConvert/Program.cs
@@ -19,7 +19,7 @@ namespace HeicConvert
 
 			const string inFormat = "HEIC";
 			string outFormat = args[0].ToUpper();
-			int threadsAmount = 4;
+			int threadsAmount = args.Length == 3 ? int.Parse(args[2]) : 1;
 
 			var dir = Directory.CreateDirectory(args[1]);
 			FileInfo[] files = dir.GetFiles()
@@ -49,61 +49,85 @@ namespace HeicConvert
 
 				taskQueue.Enqueue( (file, outFileName) );
 			}
-			Thread[] threads = new Thread[threadsAmount];
-			for (int i = 0; i < threadsAmount; i++)
+
+			MagickFormat format;
+			switch (outFormat)
 			{
-				threads[i] = new Thread(new ThreadStart( () => {
-					while (true)
-					{
-						(FileInfo, string) localItem;
-						lock (taskQueue)
-						{
-							if (taskQueue.Count == 0)
-								break;
-							else
-							{
-								localItem = taskQueue.Dequeue();
-							}
-						}
-
-						MagickFormat format;
-						switch (outFormat)
-						{
-							case "JPEG": 
-								format = MagickFormat.Jpeg;
-								break;
-							case "PNG": 
-								format = MagickFormat.Png00;
-								break;
-							case "JPG": 
-							default:
-								format = MagickFormat.Jpg;
-								break;
-						}
-
-						using (var image = new MagickImage(localItem.Item1, MagickFormat.Heic))
-						{
-							image.Write(new FileInfo(localItem.Item2), format);
-						}
-						pbar.Tick();
-					}
-				}));
-				threads[i].Start();
+				case "JPEG": 
+					format = MagickFormat.Jpeg;
+					break;
+				case "PNG": 
+					format = MagickFormat.Png00;
+					break;
+				case "JPG": 
+				default:
+					format = MagickFormat.Jpg;
+					break;
 			}
 
-			foreach (var thread in threads)
+			if (threadsAmount == 1)
 			{
-				thread.Join();
+				while (taskQueue.Count > 0)
+				{
+					var entry = taskQueue.Dequeue();
+
+					using (var image = new MagickImage(entry.file.FullName, MagickFormat.Heic))
+					{
+						image.Write(entry.outputFileName, format);
+					}
+					pbar.Tick();
+				}
+			}
+			else
+			{
+				Thread[] threads = new Thread[threadsAmount];
+				for (int i = 0; i < threadsAmount; i++)
+				{
+					threads[i] = new Thread(new ThreadStart( () => {
+						while (true)
+						{
+							(FileInfo, string) localItem;
+							lock (taskQueue)
+							{
+								if (taskQueue.Count == 0)
+									break;
+								else
+								{
+									localItem = taskQueue.Dequeue();
+								}
+							}
+
+							
+							try
+							{
+								using (var image = new MagickImage(localItem.Item1, MagickFormat.Heic))
+								{
+									image.Write(localItem.Item2, format);
+								}
+							}
+							finally
+							{
+								pbar.Tick();
+							}
+						}
+					}));
+					threads[i].Start();
+				}
+
+				foreach (var thread in threads)
+				{
+					thread.Join();
+				}
 			}
 
 			Environment.Exit(0);
 		}
 		private static void CheckArgs(string[] args)
 		{
-			if ( args.Length != 2)
+			if ( args.Length < 2 || args.Length > 3)
 			{
 				Console.WriteLine("Invalid number of arguments.");
-				Console.WriteLine("Needed arguments: output format [jpg (default), jpeg, png], directory path");
+				Console.WriteLine("Needed arguments: output format [jpg (default), jpeg, png], directory path, thread amount (default=1)");
 				Environment.Exit(1);
 			}
 
@@ -121,6 +145,25 @@ namespace HeicConvert
 				Console.WriteLine("Check the directory name and try again, please.");
 				Environment.Exit(1);
 			}
+
+			if ( args.Length == 3)
+			{
+				if ( int.TryParse(args[2], out int t))
+				{
+					if ( t <= 0)
+					{
+						Console.WriteLine($"Invalid number of threads: {t}");
+						Console.WriteLine("Please choose a positive integer");
+						Environment.Exit(1);
+					}
+				}
+				else
+				{
+					Console.WriteLine($"Invalid number of threads: {t}");
+					Console.WriteLine("Please choose a positive integer");
+					Environment.Exit(1);
+				}
+			} 
 		}
 	}
 }
